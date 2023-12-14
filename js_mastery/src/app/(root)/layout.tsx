9e80bdf5c43550cf3210e7d0c4b6101640d83b83
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-14 09:43:41 -0300

    [ :art: ] | update: layout - root (app)

--- js_mastery/src/app/(root)/layout.tsx
+++ js_mastery/src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { NavBar } from "@/components/NavBar";
 
 export default function layout({ children }: { children: React.ReactNode }) {
@@ -5,6 +6,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
         <>
             <NavBar/>
             {children}
+            <Footer/>
         </>
     );
 }
