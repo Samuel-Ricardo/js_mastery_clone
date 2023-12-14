3e444d90695df4ecb80c142875492f9fbd719aac
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-14 11:03:16 -0300

    [ :mag_right: ] | create: util - buildQuery (sanity)

---
+++ js_mastery/sanity/utils.ts
@@ -0,0 +1,28 @@
+import qs from 'querystring'
+
+interface BuildQueryParams {
+    type: string;
+    query: string;
+    category: string;
+    page: number;
+    perPage: number;
+}
+
+export function buildQuery(params: BuildQueryParams) {
+    const { type, query, category, page = 1, perPage = 20 } = params
+    
+    const conditions = [`*[_type=="${type}"]`]
+
+    if(query) conditions.push(`title match "*${query}*" `)
+    
+    if(category && category !== 'all') conditions.push(`category == "${category}"`)
+
+    const offset = (page - 1) * perPage;
+    const limit = perPage;
+
+    return conditions.length > 1 
+    ? `${conditions[0]} && (${conditions
+        .slice(1)
+        .join(" && ")})][${offset}...${limit}]`
+    : `${conditions[0]}][${offset}...${limit}]`;
+}
