25e5e364750c74b1eef419555644b1f10f5e2707
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-14 09:36:47 -0300

    [ :art: ] | update: component - navbar (app)

--- js_mastery/src/components/NavBar.tsx
+++ js_mastery/src/components/NavBar.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const NavBar = () => {
+export const NavBar = () => {
   return (
     <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 bg-black-100 py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
-        <link href="/">
+        <Link href="/">
           <Image
             src="/jsm-logo.svg"
             alt="JavaScript Mastery Logo"
             width={55}
             height={40}
           />
-        </link>
+        </Link>
 
         <Image
           src="/hamburger-menu.svg"
@@ -24,13 +24,19 @@ const NavBar = () => {
 
         <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
           <li className="body-text text-gradient_blue-purple !font-bold">
-            <Link href="https://jsmastery.pro/next13" target="_blank">
+            <Link
+              href="https://jsmastery.pro/next13"
+              target="_blank"
+            >
               Next.js Course
             </Link>
           </li>
-          <li className="body-text text-gradient_blue-purple !font-bold">
-            <Link href="https://jsmastery.pro/next13" target="_blank">
-              Next.js Course
+          <li className="body-text !font-normal">
+            <Link
+              href="https://jsmastery.pro/masterclass"
+              target="_blank"
+            >
+              Masterclass
             </Link>
           </li>
         </ul>
