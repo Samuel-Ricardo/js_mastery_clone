0be5f110808dca18468e1f348f75e9d42338d9d8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-15 10:18:21 -0300

    [ :art: ] | create: component - searchForm (app)

--- js_mastery/src/components/SearchForm.tsx
+++ js_mastery/src/components/SearchForm.tsx
@@ -3,6 +3,8 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { formUrlQuery } from "../../sanity/utils";
+import Image from "next/image";
+import { Input } from "./ui/input";
 
 export const SearchForm = () => {
   const searchParams = useSearchParams();
@@ -34,6 +36,23 @@ export const SearchForm = () => {
   }, [search]);
 
   return (
-    <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5"></form>
+    <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
+      <label className="flex-center relative w-full max-w-3xl">
+        <Image
+          src="/magnifying-glass.svg"
+          className="absolute left-8"
+          width={32}
+          height={32}
+          alt="Search Icon"
+        />
+        <Input
+          className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </label>
+    </form>
   );
 };
