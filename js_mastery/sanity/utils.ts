bd8f225a83e5628bf532bb972f28d34cb9152cb4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-15 10:04:25 -0300

    [ :sparkles: ] | create: util - formUrlQuery (app)

--- js_mastery/sanity/utils.ts
+++ js_mastery/sanity/utils.ts
@@ -1,28 +1,56 @@
-import qs from 'querystring'
+import qs from "query-string";
 
 interface BuildQueryParams {
-    type: string;
-    query: string;
-    category: string;
-    page: number;
-    perPage: number;
+  type: string;
+  query: string;
+  category: string;
+  page: number;
+  perPage: number;
 }
 
 export function buildQuery(params: BuildQueryParams) {
-    const { type, query, category, page = 1, perPage = 20 } = params
-    
-    const conditions = [`*[_type=="${type}"]`]
+  const { type, query, category, page = 1, perPage = 20 } = params;
 
-    if(query) conditions.push(`title match "*${query}*" `)
-    
-    if(category && category !== 'all') conditions.push(`category == "${category}"`)
+  const conditions = [`*[_type=="${type}"]`];
 
-    const offset = (page - 1) * perPage;
-    const limit = perPage;
+  if (query) conditions.push(`title match "*${query}*" `);
 
-    return conditions.length > 1 
+  if (category && category !== "all")
+    conditions.push(`category == "${category}"`);
+
+  const offset = (page - 1) * perPage;
+  const limit = perPage;
+
+  return conditions.length > 1
     ? `${conditions[0]} && (${conditions
         .slice(1)
         .join(" && ")})][${offset}...${limit}]`
     : `${conditions[0]}][${offset}...${limit}]`;
+}
+
+interface UrlQueryParams {
+  params: string;
+  key?: string;
+  value?: string | null;
+  keysToRemove?: string[];
+}
+
+export function formUrlQuery({
+  params,
+  key,
+  value,
+  keysToRemove,
+}: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  if (keysToRemove) {
+    keysToRemove.forEach((key) => delete currentUrl[key]);
+  } else if (key && value) {
+    currentUrl[key] = value;
+  }
+
+  return qs.stringifyUrl(
+    { url: window.location.pathname, query: currentUrl },
+    { skipNull: true }
+  );
 }
