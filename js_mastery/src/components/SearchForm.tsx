7ad3d35e0eac239b501d8bc063106bcfe6ee4ffc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-15 10:12:28 -0300

    [ :sparkles: ] | create: system - search (app/component)

--- js_mastery/src/components/SearchForm.tsx
+++ js_mastery/src/components/SearchForm.tsx
@@ -1,34 +1,39 @@
-'use client'
+"use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import {} from '@sanity/util'
+import { formUrlQuery } from "../../sanity/utils";
 
 export const SearchForm = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
 
-    const searchParams = useSearchParams();
-    const router = useRouter();
-    const pathname = usePathname();
+  const [search, setSearch] = useState("");
 
-    const [search, setSearch] =useState("");
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
 
-    useEffect(() => {
-        
-        const delayDebounceFn = setTimeout(() => {
-            
-            let newUrl = '';
+      if (search) {
+        newUrl = formUrlQuery({
+          params: searchParams?.toString() || "",
+          key: "query ",
+          value: search,
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: searchParams?.toString() || "",
+          keysToRemove: ["query"],
+        });
+      }
+      router.push(newUrl, { scroll: false });
+    }, 300);
 
-            if (search) {
-                newUrl = formUrlQuery
-            }
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
 
-        }, 300)
-
-    })
-
-    return (
-        <form>
-
-        </form>
-    )
-}
+  return (
+    <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5"></form>
+  );
+};
