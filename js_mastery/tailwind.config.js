3e4ba12c744bb6aac463e58638d46efba9e85fcd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-15 23:15:23 -0300

    [ :art: ] | update: style - config > font (tailwind)

--- js_mastery/tailwind.config.js
+++ js_mastery/tailwind.config.js
@@ -25,16 +25,17 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
+        
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
