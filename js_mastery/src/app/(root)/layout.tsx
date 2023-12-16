93090fe3af187107c849ca405ebd7056d5e3777d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-16 01:46:25 -0300

    [ :art: ] | create: loading screen (app)

--- js_mastery/src/app/(root)/layout.tsx
+++ js_mastery/src/app/(root)/layout.tsx
@@ -1,10 +1,13 @@
 import { Footer } from "@/components/Footer";
 import { NavBar } from "@/components/NavBar";
+import { Suspense } from "react";
+import Loading from "./(home)/loading";
 
 export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <NavBar/>
+            <Suspense fallback={<Loading/>}/>
             {children}
             <Footer/>
         </>
