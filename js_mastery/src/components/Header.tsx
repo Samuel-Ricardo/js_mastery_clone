210102d44c36b4673bbd3d3f1adfa7dae3ab31cd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-16 01:26:12 -0300

    [ :art: ] | create: component - Header (app)

---
+++ js_mastery/src/components/Header.tsx
@@ -0,0 +1,36 @@
+interface Props {
+    query: string;
+    category: string;
+  }
+  
+  const Header = ({ query, category }: Props) => {
+    if(query && category) {
+      return (
+        <h1 className="heading3 self-start text-white-800">
+          Search results for &quot;{query}&#34; in <span className="capitalize">{category}</span>
+        </h1>
+      )
+    }
+  
+    if(query) {
+      return (
+        <h1 className="heading3 self-start text-white-800">
+          Search results for &#34;{query}&quot;
+        </h1>
+      )
+    }
+  
+    if(category) {
+      return (
+        <h1 className="heading3 self-start text-white-800">
+          <span className="capitalize">{category}</span>
+        </h1>
+      )
+    }
+  
+    return (
+      <h1 className="heading3 self-start text-white-800">No Results</h1>
+    )
+  }
+  
+  export default Header
